26d86fe3ed9e2bd1b089468b6861da81e12f51f6
John <jsnd11@gmail.com>
2025-11-19 15:43:46 -0500

    Clean up server.js by removing commented code
    Removed commented-out code related to server setup and database connection.

--- server.js
+++ server.js
@@ -1,31 +1,5 @@
-// //CURRENTLY DEPLOYED ON RENDER
-// const express = require("express");
-// const app = express();
-// const cors = require("cors");
-// require("dotenv").config({ path: "./config.env" });
-// const port = process.env.PORT || 5000;
-// app.use(cors());
-// app.use(express.json());
-// // app.use(require("./routes/record"));
-// app.use("/record", require("./routes/record"));
+//CURRENTLY DEPLOYED ON RENDER
 
-// // get driver connection
-// const dbo = require("./db/conn");
-
-// async function startServer() {
-//   try {
-//     await dbo.connectToServer(); // Wait until the database is connected
-//     console.log(`Server is running on port: ${port}`);
-//     app.listen(port); // Only start the server after DB connection is successful
-//   } catch (err) {
-//     console.error("Error starting server:", err);
-//     process.exit(1); // Exit the process if DB connection fails
-//   }
-// }
-
-// startServer();
-
-// CURRENTLY DEPLOYED ON RENDER
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config({ path: "./config.env" });
